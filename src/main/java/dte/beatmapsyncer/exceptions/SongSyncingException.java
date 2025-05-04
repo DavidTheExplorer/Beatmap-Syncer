0a2d50ff87fe35e9844bf7744d87a2bdc9c3e0dd
David <darknight4123@gmail.com>
2025-05-04 05:32:06 +0300

    Removed lombok

--- src/main/java/dte/beatmapsyncer/exceptions/SongSyncingException.java
+++ src/main/java/dte/beatmapsyncer/exceptions/SongSyncingException.java
@@ -2,14 +2,19 @@ package dte.beatmapsyncer.exceptions;
 
 import java.io.File;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
-@Getter
 public class SongSyncingException extends RuntimeException
 {
 	private final File songFolder;
 	
 	private static final long serialVersionUID = -3669683519692752337L;
+
+	public SongSyncingException(File songFolder)
+	{
+		this.songFolder = songFolder;
+	}
+
+	public File getSongFolder()
+	{
+		return this.songFolder;
+	}
 }
