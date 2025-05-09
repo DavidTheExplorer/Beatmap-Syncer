22dda28f7dff1c04ea7d8bdc1091bbe2f33985c5
David <darknight4123@gmail.com>
2025-05-09 09:28:28 +0300

    Switched to my picocli fork

--- src/main/java/dte/beatmapsyncer/Main.java
+++ src/main/java/dte/beatmapsyncer/Main.java
@@ -1,16 +1,25 @@
 package dte.beatmapsyncer;
 
-import dte.beatmapsyncer.cli.BeatmapSyncerDefaultProvider;
 import dte.beatmapsyncer.exceptions.LoggerExceptionHandler;
+import dte.beatmapsyncer.utils.OSUtils;
 import picocli.CommandLine;
+import picocli.CommandLine.IDefaultValueProvider;
+import picocli.IDefaultValueProviderBuilder;
 
 public class Main
 {
     public static void main(String[] args)
     {
         System.exit(new CommandLine(new BeatmapSyncer())
-                .setDefaultValueProvider(new BeatmapSyncerDefaultProvider())
+                .setDefaultValueProvider(createDefaultValueProvider())
                 .setExecutionExceptionHandler(new LoggerExceptionHandler())
                 .execute(args));
+    }
+
+    private static IDefaultValueProvider createDefaultValueProvider()
+    {
+        return new IDefaultValueProviderBuilder()
+                .forOption("-gameFolder", OSUtils.getGameFolder().toString())
+                .build();
     }
 }
