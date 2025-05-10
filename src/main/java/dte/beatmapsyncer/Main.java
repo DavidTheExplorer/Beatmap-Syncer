c22cada0b3de81c91359238a92e3fadc6b5859b2
David <darknight4123@gmail.com>
2025-05-10 22:35:33 +0300

    Added a windows check at startup

--- src/main/java/dte/beatmapsyncer/Main.java
+++ src/main/java/dte/beatmapsyncer/Main.java
@@ -3,14 +3,23 @@ package dte.beatmapsyncer;
 import dte.beatmapsyncer.exceptions.LoggerExceptionHandler;
 import dte.beatmapsyncer.exceptions.SimpleParameterExceptionHandler;
 import dte.beatmapsyncer.utils.OSUtils;
+import dte.beatmapsyncer.utils.OperatingSystem;
 import picocli.CommandLine;
 import picocli.CommandLine.IDefaultValueProvider;
 import picocli.IDefaultValueProviderBuilder;
+
+import static dte.beatmapsyncer.utils.OperatingSystem.WINDOWS;
 
 public class Main
 {
     public static void main(String[] args)
     {
+        if(OperatingSystem.detectCurrent() != WINDOWS)
+        {
+            System.err.println("Error: BeatmapSyncer is currently supported only on Windows.");
+            return;
+        }
+
         System.exit(new CommandLine(new BeatmapSyncer())
                 .setDefaultValueProvider(createDefaultValueProvider())
                 .setExecutionExceptionHandler(new LoggerExceptionHandler())
