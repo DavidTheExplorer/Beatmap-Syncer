8ea95aa900738beb36257ee76dd91a84dbccac10
David <darknight4123@gmail.com>
2025-05-20 23:35:33 +0300

    The syncing process now uses a progress bar
    This avoids printing a huge amount of beatmap names.

--- beatmapsyncer-cli/src/main/java/dte/beatmapsyncer/cli/BeatmapSyncerCLI.java
+++ beatmapsyncer-cli/src/main/java/dte/beatmapsyncer/cli/BeatmapSyncerCLI.java
@@ -5,6 +5,8 @@ import dte.beatmapsyncer.beatmap.BeatmapScanner;
 import dte.beatmapsyncer.beatmap.syncer.BeatmapSyncer;
 import dte.beatmapsyncer.beatmap.syncer.BeatmapSyncer.Context;
 import dte.beatmapsyncer.beatmap.syncer.LocalBeatmapSyncer;
+import me.tongfei.progressbar.ProgressBar;
+import me.tongfei.progressbar.ProgressBarBuilder;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Model.CommandSpec;
 import picocli.CommandLine.Option;
@@ -19,6 +21,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.concurrent.Callable;
 
+import static me.tongfei.progressbar.ProgressBarStyle.ASCII;
 import static picocli.CommandLine.ExitCode.OK;
 
 @Command(name = "beatmapsyncer", description = "Tracks your changed osu! beatmaps so they are updated on every machine you play on.")
@@ -64,10 +67,7 @@ public class BeatmapSyncerCLI implements Callable<Integer>
 
         System.out.printf("%d beatmaps were found since the last sync! (%s)%n", unsyncBeatmaps.size(), SYNC_DATE_DISPLAY_FORMATTER.format(lastSyncDate));
         System.out.println("Syncing started...");
-
-        System.out.println();
         sync(unsyncBeatmaps);
-        System.out.println();
         System.out.println("Success!");
         return OK;
     }
@@ -91,30 +91,18 @@ public class BeatmapSyncerCLI implements Callable<Integer>
 
     private void sync(List<Beatmap> unsyncBeatmaps)
     {
-        int longestBeatmapName = unsyncBeatmaps.stream()
-                .mapToInt(beatmap -> beatmap.name().length())
-                .max()
-                .getAsInt(); //safe - this method is not called when the list is empty
-
-        String separator = "-".repeat(18 + longestBeatmapName + String.valueOf(unsyncBeatmaps.size()).length());
-
-        System.out.println(separator);
-
         BeatmapSyncer.Context context = new Context(LocalDateTime.now());
 
-        for(int i = 0; i < unsyncBeatmaps.size(); i++)
-        {
-            Beatmap beatmap = unsyncBeatmaps.get(i);
-
-            System.out.printf("Syncing \"%s\"%s(%d/%d)%n",
-                    beatmap.name(),
-                    " ".repeat(longestBeatmapName - beatmap.name().length() + 5),
-                    i+1,
-                    unsyncBeatmaps.size());
-
+        for(Beatmap beatmap : ProgressBar.wrap(unsyncBeatmaps, createProgressBarBuilder()))
             this.beatmapSyncer.sync(beatmap, context);
-        }
+    }
 
-        System.out.println(separator);
+    private static ProgressBarBuilder createProgressBarBuilder()
+    {
+        return new ProgressBarBuilder()
+                .setTaskName("Syncing Beatmaps")
+                .setStyle(ASCII)
+                .hideEta()
+                .setUpdateIntervalMillis(1);
     }
 }
